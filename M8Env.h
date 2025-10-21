0ba3b9fc0888c680e1e491e9d8aacbcb5f8bd719
Andrew Brown <algorithmicmusic@gmail.com>
2025-10-21 17:28:43 +1000

    Added env max gain control

--- M8Env.h
+++ M8Env.h
@@ -42,7 +42,7 @@ public:
   uint8_t next(uint8_t sample) {
     if (attack_stage) { //attack;
       env_val += attack; 
-      if (env_val >= 1.0) attack_stage = false;
+      if (env_val >= env_gain) attack_stage = false;
       return round(sample * env_val);
     } else { // decay
       env_val *= decay;
@@ -54,6 +54,10 @@ public:
     return env_val;
   }
 
+  void setGain(float val) {
+    env_gain = min(1.0f, max(0.0f, val));
+  }
+
 private:
   float attack = 0;
   float raw_attack = 0;
@@ -61,6 +65,7 @@ private:
   float raw_decay = 0.5;
   bool attack_stage = false;
   float env_val = 0.01;
+  float env_gain = 1.0;
 
 };
   
